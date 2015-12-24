35203d7bb430f07ba196bf000d29633c1bb79a8b
azure provisioned user <Git_test@gittest.gittest-t873bztk.b10.internal.cloudapp.net>
2015-12-24 05:53:14 +0000

    hack-dat-kiwi-ctf-2015

--- 0ctf-2015/web/mislead/202.112.28.110/mislead/register.php
+++
@@ -1,7 +0,0 @@
-<form name="register" action="" method="post">
-Username: 
-<input type="text" name="username" />
-Password:
-<input type="text" name="password" />
-<input type="submit" name="submit" value="Submit" />
-</form>
