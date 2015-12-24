e7dabda27707be046ca611df4daa688708758979
azure provisioned user <Git_test@gittest.gittest-t873bztk.b10.internal.cloudapp.net>
2015-12-24 05:50:38 +0000

    uiuctf-2015

--- rctf-quals-2015/web/easysql/180.76.178.54_8002/2abb0f5175a93369a0cd07716bed554e/login.php
+++
@@ -1,1 +0,0 @@
-<form action="login.php" method="post"><p>username: <input type="text" name="username" /></p><p>password: <input type="text" name="password" /></p><input type="submit" value="Submit" /></form>
--- rctf-quals-2015/web/easysql/180.76.178.54_8002/2abb0f5175a93369a0cd07716bed554e/register.php
+++
@@ -1,1 +0,0 @@
-<form action="register.php" method="post"><p>username: <input type="text" name="username" /></p><p>password: <input type="text" name="password" /></p>email: <input type="text" name="email" /></p><input type="submit" value="Submit" /></form> 
