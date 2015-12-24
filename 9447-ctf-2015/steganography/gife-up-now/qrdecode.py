74997e1e535479a00e9ca773091c7fbd1f81c97f
azure provisioned user <Git_test@gittest.gittest-t873bztk.b10.internal.cloudapp.net>
2015-12-24 05:54:23 +0000

    hitcon-ctf-quals-2015

--- 9447-ctf-2015/steganography/gife-up-now/qrdecode.py
+++
@@ -1,7 +0,0 @@
-import sys, zbarlight
-from PIL import Image
-with open(sys.argv[1],'rb') as f:
-	image = Image.open(f)
-	image.load()
-res = zbarlight.scan_codes('qrcode',image)
-print sys.argv[1],res
