604d76aa4c0b9e9880becfd248a08d3621953c8e
azure provisioned user <Git_test@gittest.gittest-t873bztk.b10.internal.cloudapp.net>
2015-12-24 05:50:20 +0000

    pragyan-ctf-2015

--- asis-quals-ctf-2015/stego/blind/pixels.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import sys
-im = Image.open(sys.argv[1])
-width, height = im.size
-for h in range(height):
-	for w in range(width):
-		r,g,b,a = im.getpixel((w,h))
-		print b,
-	print
--- backdoor-ctf-2015/crypto/rsalot/genmods.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-for i in pems/*.pem; do MOD="`openssl rsa -pubin -inform PEM -noout -modulus < $i`"; echo "${MOD:8}" > "mods/$(basename $i).mod"; done
